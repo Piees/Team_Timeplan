aab88ab3dfe44c24f95bdf0231cc59c70aa28cf1
eanilsen <evenanilsen@gmail.com>
2015-02-19 19:35:57 +0100

    Prøver å forenkle lab2

--- lab2/romannum.py
+++ lab2/romannum.py
@@ -1,10 +1,27 @@
-roman_val = (('M', 1000),
-            ('D', 500),
-            ('C', 100),
-            ('L', 50),
-            ('X', 10),
-            ('V', 5),
-            ('I', 1))
+romanNumeralMap = (('M',  1000),
+                   ('CM', 900),
+                   ('D',  500),
+                   ('CD', 400),
+                   ('C',  100),
+                   ('XC', 90),
+                   ('L',  50),
+                   ('XL', 40),
+                   ('X',  10),
+                   ('IX', 9),
+                   ('V',  5),
+                   ('IV', 4),
+                   ('I',  1))
 
-def to_roman(dec):
-    passpython re
+def to_roman(n):
+    """convert integer to Roman numeral"""
+    if not (0 < n < 5000):
+        raise OutOfRangeError, "number out of range (must be 1..4999)"
+    if int(n) != n:
+        raise NotIntegerError, "decimals can not be converted"
+
+    result = ""
+    for numeral, integer in romanNumeralMap:
+        while n >= integer:
+            result += numeral
+            n -= integer
+    return result
